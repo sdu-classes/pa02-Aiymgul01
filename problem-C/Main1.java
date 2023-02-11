012497e6fee5b316cfde9ea0856f253bbf91305e
Aiymgul01 <124436362+Aiymgul01@users.noreply.github.com>
2023-02-12 04:36:13 +0600

    Create Main1.java

---
+++ problem-C/Main1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class People {
+    public static void main(String[]args){
+        Scanner scan =new Scanner(System.in);
+        String name = scan.next();
+        String address = scan.next();
+        String program = scan.next();
+        int year = scan.nextInt();
+        double fee = scan.nextDouble();
+        String school = scan.next();
+        double pay = scan.nextDouble();
+        Person p = new Person(name,address);
+        Student1 s = new Student1(name, address, program, year, fee);
+        System.out.println(p);
+        System.out.println(s);
+        Staff st = new Staff(name, address, school, pay);
+        System.out.println(st);
+
+    }
+}
