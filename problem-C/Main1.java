4e42fc6de8f623452a7717ed4b627f552a9014bc
Aiymgul01 <124436362+Aiymgul01@users.noreply.github.com>
2023-02-12 05:22:43 +0600

    Update Main1.java

--- problem-C/Main1.java
+++ problem-C/Main1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class Main1 {
     public static void main(String[]args){
         Scanner scan =new Scanner(System.in);
         String name = scan.next();
