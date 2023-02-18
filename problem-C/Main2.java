28ef1058ba72b3d8f9f103099d0d26db93878894
Aiymgul01 <124436362+Aiymgul01@users.noreply.github.com>
2023-02-18 16:00:17 +0600

    Update and rename Main1.java to Main2.java

--- problem-C/Main2.java
+++ problem-C/Main2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main1 {
+public class Main2 {
     public static void main(String[]args){
         Scanner scan =new Scanner(System.in);
         String name = scan.next();
